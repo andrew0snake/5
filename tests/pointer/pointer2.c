e18b24cdfff7cf7d3208756ed234042d9112adf5
Andrew0snake <andrew000snake@gmail.com>
2016-08-03 08:29:27 +0700

    20160803

--- tests/pointer/pointer2.c
+++ tests/pointer/pointer2.c
@@ -12,20 +12,30 @@ void main () {
     int i = 0;
 
     int *p1;
+//    double *p1;
 
     for ( i = 0; i < SIZE; i++ ) {
         arr1 [ i ] = i * 7;
         printf ( "at step %d arr1 [ %d ] = %d;\n", i, i, arr1 [ i ] );
     }
     
-    p1 = & arr1 [ 1 ];
-    printf ( "p1 = %d; and *p1 = %d;\n", p1, *p1 );
-    printf ( "p1 + 1 = %d; and * ( p1 + 1 ) = %d;\n", p1 + 1, * ( p1 + 1 ) );
+    p1 = & arr1 [ 0 ];
+    printf ( "\np1 = %p; and *p1 = %d;\n\n", p1, *p1 );
+    printf ( "p1 + 1 = %p; and * ( p1 + 1 ) = %d;\n\n", p1 + 1, * ( p1 + 1 ) );
 
-    for () {
+    printf ( "p1 + 5 = %p; and * ( p1 + 5 ) = %d;\n\n", p1 + 5, * ( p1 + 5 ) );
 
+    p1 = arr1;
+    printf ( "p1 = a = %p;\n\n", p1 );
+
+    i = 1;
+    printf ( "arr1 [ i = 1 ] = %d; * ( arr1 + i = arr1 + 1 ) = %d;\n\n", arr1 [ i ], * ( arr1 + i ) );
+
+
+/*    for () {
+        printf ( "" );
     };
-
+*/
 }
 
 
